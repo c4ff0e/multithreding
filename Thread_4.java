bdbcfea35ef59453ed72f2ac2d5fd106f939142f
c4ff0e <76472853+c4ff0e@users.noreply.github.com>
2025-10-07 01:05:34 +0300

    var7 case1 done in thread 4

--- Thread_4.java
+++ Thread_4.java
@@ -9,6 +9,22 @@ public class Thread_4 extends Thread {
     }
     @Override
     public void run() {
+        Integer firstEven = null;
         System.out.println("Hello World от потока №" + threadnumber + " " + randomMassive[0]);
+        for (int i = 0; i < randomMassive.length; i++) {
+            int value = randomMassive[i];
+            if (value % 2 != 0) {
+                continue;} 
+
+           if (firstEven == null) {
+            firstEven = value;
+            continue;
+           }
+           int sum = firstEven + value;
+           System.out.println(sum);
+           firstEven = null;
+        }
+        
     }
 }
+
